432cdd79ad6a5036f007d1c79190073330f75087
KaiChiehKJ <timothychang.kj@gmail.com>
2025-02-27 10:20:55 +0800

    Update GISshape.py
    Revised the output columns of the function "get_OD_loine_shp".

--- GISshape.py
+++ GISshape.py
@@ -61,11 +61,7 @@ def get_OD_line_shp(df, o_col, d_col, o_x_col, o_y_col, d_x_col, d_y_col, count_
     countdf (dataframe):統計出來的OD表 (尚未轉成geodataframe)
     countgdf　(geodataframe):為WGS84的geodataframe
     '''
-    # import numpy as np
-    # import pandas as pd
-    # import geopandas as gpd
-    # from shapely.geometry import LineString
-    
+
     def get_line(df, x1 = o_x_col, x2 = d_x_col, y1 = o_y_col, y2 = d_y_col):
         df['geometry'] = df.apply(lambda row: LineString([(row[x1], row[y1]), (row[x2], row[y2])]), axis=1)
         gdf = gpd.GeoDataFrame(df, geometry='geometry')
@@ -96,8 +92,13 @@ def get_OD_line_shp(df, o_col, d_col, o_x_col, o_y_col, d_x_col, d_y_col, count_
         df_copy['Pair'] = np.where(df_copy['PlaceID_o'] < df_copy['PlaceID_d'],
                             df_copy['PlaceID_o'].astype(str) + '-' + df_copy['PlaceID_d'].astype(str),
                             df_copy['PlaceID_d'].astype(str) + '-' + df_copy['PlaceID_o'].astype(str))
+        
+        df_copy['PlacePair'] = np.where(df_copy['PlaceID_o'] < df_copy['PlaceID_d'],
+                        df_copy[o_col].astype(str) + '-' + df_copy[d_col].astype(str),
+                        df_copy[d_col].astype(str) + '-' + df_copy[o_col].astype(str))
+
         if how != 'countd':
-            countdf = df_copy.groupby(['Pair'], as_index=False).agg({count_col: 'sum'}).sort_values(count_col,ascending=False).reset_index(drop = True) # Groupby 並計算總和
+            countdf = df_copy.groupby(['PlacePair','Pair'], as_index=False).agg({count_col: 'sum'}).sort_values(count_col,ascending=False).reset_index(drop = True) # Groupby 並計算總和
             countdf[['PlaceID1', 'PlaceID2']] = countdf['Pair'].str.split('-', expand=True).astype(int) # 拆分 'Pair' 欄位為 PlaceID1 和 PlaceID2 並轉為 int 類型
             
             # 準備 place 資料表的 PlaceID 對應座標，只處理一次
@@ -109,14 +110,14 @@ def get_OD_line_shp(df, o_col, d_col, o_x_col, o_y_col, d_x_col, d_y_col, count_
                             .merge(place_rename2, on='PlaceID2', suffixes=['_o', '_d'])
             
             # 篩選所需欄位並重新命名
-            countdf = countdf[['Pair', count_col, 'PlaceLng_o', 'PlaceLat_o', 'PlaceLng_d', 'PlaceLat_d']]\
+            countdf = countdf[['PlacePair', count_col, 'PlaceLng_o', 'PlaceLat_o', 'PlaceLng_d', 'PlaceLat_d']]\
                     .rename(columns={'PlaceLng_o': o_x_col, 
                                         'PlaceLat_o': o_y_col, 
                                         'PlaceLng_d': d_x_col, 
                                         'PlaceLat_d': d_y_col})
         elif how == 'countd':
             
-            countdf = df_copy.groupby(['Pair'], as_index=False).agg({count_col: 'sum',date_col:'nunique'})
+            countdf = df_copy.groupby(['Pair','PlacePair'], as_index=False).agg({count_col: 'sum',date_col:'nunique'})
 
             countdf[count_col] = countdf[count_col] / countdf[date_col]
             
@@ -130,12 +131,12 @@ def get_OD_line_shp(df, o_col, d_col, o_x_col, o_y_col, d_x_col, d_y_col, count_
             countdf = countdf.merge(place_rename, on='PlaceID1')\
                             .merge(place_rename2, on='PlaceID2', suffixes=['_o', '_d'])
             # 篩選所需欄位並重新命名
-            countdf = countdf[['Pair', count_col, 'PlaceLng_o', 'PlaceLat_o', 'PlaceLng_d', 'PlaceLat_d']]\
+            countdf = countdf[['PlacePair', count_col, 'PlaceLng_o', 'PlaceLat_o', 'PlaceLng_d', 'PlaceLat_d']]\
                     .rename(columns={'PlaceLng_o': o_x_col, 
                                         'PlaceLat_o': o_y_col, 
                                         'PlaceLng_d': d_x_col, 
                                         'PlaceLat_d': d_y_col})
-    
+
     countgdf = get_line(countdf)
     return countgdf
 
