94a24f1cb3d0cf4c90e48256e530fb1aed0a0126
KaiChiehKJ <timothychang.kj@gmail.com>
2025-03-03 10:21:54 +0800

    Create ProcessBasic.py

---
+++ ProcessBasic.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import os 
+import shutil
+
+def create_folder(folder_name):
+    """建立資料夾"""
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    return os.path.abspath(folder_name)
+
+def delete_folders(deletelist):
+    """
+    刪除資料夾
+    deletelist(list):需要為皆為路徑的list
+    """
+    for folder_name in deletelist: 
+        if os.path.exists(folder_name): # 檢查資料夾是否存在
+            shutil.rmtree(folder_name) # 刪除資料夾及其內容
+        else:
+            print(f"資料夾 '{folder_name}' 不存在。")
+
+def getdatelist(time1, time2):
+    '''
+    建立日期清單
+    time1、time2(str):為%Y-%M-%D格式的日期字串
+    '''
+    if time1 > time2:
+        starttime = time2
+        endtime = time1
+    else:
+        starttime = time1
+        endtime = time2
+
+    date_range = pd.date_range(start=starttime, end=endtime)
+    datelist = [d.strftime("%Y%m%d") for d in date_range]
+    return datelist
+
+def findfiles(filefolderpath, filetype='.csv', all=True):
+    """
+    尋找指定路徑下指定類型的檔案，並返回檔案路徑列表。
+
+    Args:
+        filefolderpath (str): 指定的檔案路徑。
+        filetype (str, optional): 要尋找的檔案類型，預設為 '.csv'。
+        all (bool, optional): 是否檢索所有子資料夾，預設為 True。
+
+    Returns:
+        list: 包含所有符合條件的檔案路徑的列表。
+    """
+    filelist = []
+
+    if all:
+        # 遍歷資料夾及其子資料夾
+        for root, _, files in os.walk(filefolderpath):
+            for file in files:
+                if file.endswith(filetype):
+                    file_path = os.path.join(root, file)
+                    filelist.append(file_path)
+    else:
+        # 僅檢索當前資料夾
+        for file in os.listdir(filefolderpath):
+            file_path = os.path.join(filefolderpath, file)
+            if os.path.isfile(file_path) and file.endswith(filetype):
+                filelist.append(file_path)
+
+    return filelist
+
