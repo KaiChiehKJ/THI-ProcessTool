12a0b0db9d3577f3a04e390b0addac20af2bf2a5
KaiChiehKJ <timothychang.kj@gmail.com>
2025-03-03 15:17:57 +0800

    Update THIWebCrawler.py

--- THIWebCrawler.py
+++ THIWebCrawler.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import time
 import random
+from datetime import datetime 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
